d811b8833e947ab23cd1dae682c536a5289e9002
Indy Prieto <corepdesign@gmail.com>
2016-03-03 11:57:12 -0600

    Update TlBx.js

--- TlBx.js
+++ TlBx.js
@@ -1,14 +1,14 @@
 (function(){
   //polyfill
-  if(console.debug === undefined){console.debug=console.log}
+  if(console.debug === undefined){console.debug=console.log;}
   
   //end polyfill
   
   var init = Object.create(null,{});
   init.dbg = Object.create(null,{});
-  init.dbg.callback = function(){for(var i = 0;i<arguments.length;i++){console.debug(arguments[i])}};
-  init.dbg.falsec = function(In,customstring){if(In === false){if(customstring !== undefined){customstring+" returned false!";}else(console.warn("returned false!"));}};
+  init.dbg.callback = function(){for(var i = 0;i<arguments.length;i++){console.debug(arguments[i]);}};
+  init.dbg.falsec = function(In){if(In === false){(console.warn("returned false!"));}};
   
 tlBx=init;
-if(tlBx !== init){console.warn('tlBx.js incounterd a fatal error')}else{console.debug('this console has been intergrated with tlBx')}
+if(tlBx !== init){console.warn('tlBx.js incounterd a fatal error');}
 })();
