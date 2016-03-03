4973cd5c07ed8d872a7e1d6eaf3e00dd7fa69291
Indy Prieto <corepdesign@gmail.com>
2016-03-03 13:01:03 -0600

    Update TlBx.js

--- TlBx.js
+++ TlBx.js
@@ -3,11 +3,34 @@
   if(console.debug === undefined){console.debug=console.log;}
   
   //end polyfill
-  
+  //define init
   var init = Object.create(null,{});
+  
+  //debug
   init.dbg = Object.create(null,{});
   init.dbg.callback = function(){for(var i = 0;i<arguments.length;i++){console.debug(arguments[i]);}};
   init.dbg.falsec = function(In){if(In === false){(console.warn("returned false!"));}};
+  //end debug
+  
+  //tools
+  init.tools = Object.create(null,{});
+  init.tools.shift = function(a){
+    
+    if((arguments[1] === undefined || (arguments[1]!=="forward"&&arguments[1]!=="back"))||!(Array.isArray(arguments[0]))){
+     console.warn("missing or bad value,returning false");
+     //dat error handling tough
+     return false;
+    }
+    var d = arguments[1];
+    if(d=="back"){
+    a.push(a.shift());
+    }else if(d==="forward"){
+      a.push(a.shift());
+    }
+    
+    
+  };
+  
   
 tlBx=init;
 if(tlBx !== init){console.warn('tlBx.js incounterd a fatal error');}
